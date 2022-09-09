e877003c5f63d43fdb23fd8f7f75e6c41e410d30
SMCCZ <sabir.mondal@cloudcraftz.io>
2022-09-09 17:36:41 +0530

    Project Created

---
+++ android/app/src/main/kotlin/com/example/heartful/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.heartful
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/screens/initilization_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InitilizationScreen extends StatelessWidget {
+  const InitilizationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
